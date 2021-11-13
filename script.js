e3ad5fcaa528ba5be660e8d42e81d6b8f614afbd
gafanho <gafanho@gmail.com>
2021-11-13 21:34:43 +0000

    changes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var principal = document.getElementById("principal").value;
+
+function compute()
+{
+    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
 var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
 var interest = principal * years * rate /100;
 var year = new Date().getFullYear()+parseInt(years);
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
 }
 
 function updateRate() 
